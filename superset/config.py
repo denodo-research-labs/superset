f70f049f893fa2aa41884d5909d5c978cf1700b5
Daniel Fernández <dfernandez@denodo.com>
2025-01-10 13:16:47 +0100

    Customize Superset app name and logo

--- superset/config.py
+++ superset/config.py
@@ -307,10 +307,10 @@ AUTH_RATE_LIMIT = "5 per second"
 # GLOBALS FOR APP Builder
 # ------------------------------
 # Uncomment to setup Your App name
-APP_NAME = "Superset"
+APP_NAME = "Superset for Denodo"
 
 # Specify the App icon
-APP_ICON = "/static/assets/images/superset-logo-horiz.png"
+APP_ICON = "/static/assets/images/superset-denodo-logo-horiz.png"
 
 # Specify where clicking the logo would take the user'
 # Default value of None will take you to '/superset/welcome'
